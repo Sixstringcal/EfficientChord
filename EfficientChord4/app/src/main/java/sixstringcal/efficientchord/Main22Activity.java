e86f0343a4ab70812cd41dd4b99e88c949f1f8bd
Calvin Nielson <sixstringcal@gmail.com>
2016-03-18 20:55:32 -0600

    Merged to one app

---
+++ EfficientChord4/app/src/main/java/sixstringcal/efficientchord/Main22Activity.java
@@ -0,0 +1,16 @@
+package sixstringcal.efficientchord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static sixstringcal.efficientchord.R.layout.activity_main22;
+import static sixstringcal.efficientchord.R.layout.activity_set_chord;
+
+public class Main22Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_set_chord);
+    }
+}
--- EfficientChord4/app/src/main/java/sixstringcal/efficientchord/MainActivity.java
+++ EfficientChord4/app/src/main/java/sixstringcal/efficientchord/MainActivity.java
@@ -1,16 +1,32 @@
 package sixstringcal.efficientchord;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button newButton;
+    Button loadButton;
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        newButton = (Button) findViewById(R.id.button5);
+        newButton.setOnClickListener(this);
+        loadButton = (Button) findViewById(R.id.button6);
+        loadButton.setOnClickListener(this);
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -33,5 +49,65 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void newButtonClick() {
+        startActivity(new Intent(".SetChord"));
+    }
+
+    private void loadButtonClick(){
+        startActivity(new Intent("sixstringcal.efficientchord.LoadActivity"));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button5:
+                newButtonClick();
+                break;
+            case R.id.button6:
+                loadButtonClick();
+                break;
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://sixstringcal.efficientchord/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://sixstringcal.efficientchord/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
